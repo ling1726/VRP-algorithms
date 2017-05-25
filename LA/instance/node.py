6f54d91d7e9b9a447f37bc3d7f6175f295bcb56e
Gao Lingfan <e1633394@student.tuwien.ac.at>
2017-05-25 23:12:53 +0200

    added forgotten files

---
+++ LA/instance/node.py
@@ -0,0 +1,17 @@
+# This class SHOULD NEVER be instantiated
+# BUT logic common to both types of nodes can be placed here e.g. distance
+class Node(object):
+    def __init__(self):
+        self.id = ''
+        self.x = 0.0
+        self.y = 0.0
+        self.demand = 0.0
+        self.serviceTime = 0.0
+        self.windowStart = 0.0
+        self.windowEnd = 0.0
+
+    def __hash__(self):
+        return hash(self.id)
+
+    def __str__(self):
+        return self.id
