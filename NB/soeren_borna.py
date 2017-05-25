2bde94863ec011cd92dc0950a57dfe1336ffd150
bfeldsar <bf47667@fer.hr>
2017-05-25 22:37:21 +0200

    Fixed bugs in preprocessing

--- NB/soeren_borna.py
+++ NB/soeren_borna.py
@@ -1,4 +1,4 @@
-from instance.instance import *
+from LA.instance import *
 
 
 # Schneider: The Electric Vehicle-Routing Problem with Time
@@ -7,28 +7,43 @@ from instance.instance import *
 def preprocessing():
     forbiddenArcs = set()
     # arcs (v,w) and (w,v) are not the same because of time windows
-    for node1 in nodes:
-        for node2 in nodes:
-            if node1.id == node2.id:
+    for node1Key in nodes:
+        node1 = nodes[node1Key]
+        for node2Key in nodes:
+            if node1Key == node2Key:
                 continue
+            node2 = nodes[node2Key]
 
             if (node1.demand + node2.demand > loadCapacity) or \
                     (node1.windowStart + node1.serviceTime + getValDistanceMatrix(node1, node2) / averageVelocity >
                          node2.windowEnd) or \
                     (node1.windowStart + node1.serviceTime + getValDistanceMatrix(node1, node2) / averageVelocity +
-                         node2.serviceTime + getValDistanceMatrix(node2.id, nodes["S0"]) / averageVelocity >
+                         node2.serviceTime + getValDistanceMatrix(node2, nodes["S0"]) / averageVelocity >
                          nodes["S0"].windowEnd):
                 forbiddenArcs.add((node1.id, node2.id))
                 continue
 
-            other_chargers = set(chargers)
-            for charge1 in chargers:
-                other_chargers.remove(charge1)
-                for charge2 in other_chargers:
+            #next comparison is only if both nodes are customers
+            if node1.id.startswith("S") or node2.id.startswith("S"):
+                continue
+
+            allowedArc = False
+            for charge1Key in chargers:
+                if allowedArc:
+                    break
+                charge1 = chargers[charge1Key]
+                for charge2Key in chargers:
+                    if charge1Key == charge2Key:
+                        continue
+                    charge2 = chargers[charge2Key]
                     if (fuelConsumptionRate * (getValDistanceMatrix(charge1, node1) +
                                                    getValDistanceMatrix(node1, node2) +
-                                                   getValDistanceMatrix(node2, charge2)) > fuelCapacity):
-                        forbiddenArcs.add((node1.id, node2.id))
-                        continue
+                                                   getValDistanceMatrix(node2, charge2)) <= fuelCapacity):
+                        allowedArc = True
+                        break
+
+            if not allowedArc:
+                forbiddenArcs.add((node1.id, node2.id))
 
     return forbiddenArcs
+6
