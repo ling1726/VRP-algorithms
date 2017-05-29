6fe759a6ec76575162871fcab0bdc78715ae7c47
Gao Lingfan <e1633394@student.tuwien.ac.at>
2017-05-29 10:26:36 +0200

    modified gitignore

---
+++ NB/instance/customer.py
@@ -0,0 +1,8 @@
+from instance.node import Node
+#from node import Node #UNCOMMENT THIS & COMMENT ABOVE FOR SEQINS TO WORK
+
+# Customer class with customer specific logic
+class Customer(Node):
+    def __init__(self):
+        Node.__init__(self)
+
