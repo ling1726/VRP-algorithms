a9f93a0663391bb4a871cb23056ffa2a674cfea4
abegoli <variation.proto@gmail.com>
2017-05-31 09:13:44 +0200

    Time window checks added to Terrible. Added depot at the end of each route. Wired up Terrible and EVRPTWVerifier.jar. Use -v flag to verify solution. Note: Key error means the solution is infeasible because the instance cannot be solved without using chargers.

--- LA/TerribleSequentialInsertion.py
+++ LA/TerribleSequentialInsertion.py
@@ -1,20 +1,19 @@
 import sys
+import os
+import subprocess
 import argparse
 import logging
 import math
-from random import randint
 from route import Route
 import instance.instance as inst 
-#from LA.instance import instance as inst #IF ABOVE DOESNT WORK TRY UNCOMMENTING THIS ONE
 
-_randomPoint = None
 logging.basicConfig(level=logging.INFO)                                                                       
 logger = logging.getLogger(__name__) 
 
 class Solution(object):
     def __init__(self):
-        self.routes = [] #routes is a nested Python list [[Node..][Node..]] of Node objects
-        self.Cost = 0.0
+        self.routes = [] #routes is a Python list of Route objects [Route, Route, ...]
+        self.cost = 0.
         self.distanceMatrix = inst._distanceMatrix
         self.customers = list(inst.customers.values())
         self.chargers = list(inst.chargers.values())
@@ -30,18 +29,14 @@ class Solution(object):
             customer.setAngleFromOrigin()
         self.customers.sort(key=lambda x:x.angle)
 
-    def calculateCost(self):
-        for route in routes:
-                self.Cost += route.calculateCost()
-
     def createRoute(self):
         route = Route(self.depot)
         for customer in self.customers:
             route.feasibleInsertion(customer)
+        route.insert(self.depot) #Route must end at the depot
+        self.cost += route.getCost()
         return route        
-
         
-
     def solve(self):
         while(len(self.customers)>0):
             print(len(self.customers))
@@ -56,14 +51,17 @@ class Solution(object):
             self.customers = newCustomerList[:]
             self.routes.append(route)
 
+        
     def __str__(self):
-        routeStr = str.join('\n', ['['+str.join(',',[str(customer) for customer in route.nodes])+']' for route in self.routes])
-        return 'Number of vehicles: %d\nDistance cost: %f\nRoutes:%s' % (len(self.routes), self.Cost, routeStr)
+        routeStr = str.join('\n', [str.join(', ',[str(customer) for customer in route.nodes]) for route in self.routes])
+        return '#solution for %s\n%.3f\n%s' % (inst.filename, self.cost, routeStr)
 
+        
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Runs simple Sequential Heuristic on instance file')
     parser.add_argument('--instance', '-i', metavar='INSTANCE_FILE', required=True, help='The instance file')
+    parser.add_argument('--verify', '-v', action = 'store_true', help='Uses the EVRPTWVerifier to verify the solution')
     args = parser.parse_args()
     inst.setFileName(args.instance)
     inst.parse()
@@ -71,3 +69,10 @@ if __name__ == '__main__':
     sol.sortCustomersByAngle()
     sol.solve()
     print(sol)
+
+    if args.verify:
+        tempFile = inst.filename + '.sol'
+        with open(tempFile, mode='w') as f:
+            f.write(str(sol))
+        subprocess.call(['java', '-jar', '../data/verifier/EVRPTWVerifier.jar', inst.filename, tempFile])
+        os.remove(tempFile)
--- LA/route.py
+++ LA/route.py
@@ -6,21 +6,35 @@ class Route(object):
         self.nodes = [depot]
         self.battery = inst.fuelCapacity
         self.capacity = inst.loadCapacity
-
-    def calculateCost(self):
-        for i, point in enumerate(nodes[1:]):
-            self.cost += inst._distanceMatrix[(nodes[i].id, point.id)]
+        self.distance = 0.
+        self.time = 0.
 
     def feasibleInsertion(self, customer):
         futureCapacity = self.capacity - customer.demand
         futureBattery = self.battery - (inst._distanceMatrix[(self.nodes[-1].id, customer.id)]*inst.fuelConsumptionRate)
-        if futureBattery < 0 or futureCapacity < 0: return False
+        if futureBattery < 0 or futureCapacity < 0 or not self.timeFeasible(customer) or not self.depotReachable(customer, futureBattery): return False
         self.battery = futureBattery
         self.capacity = futureCapacity
-        self.nodes.append(customer)
+        self.insert(customer)
         return True
 
+    """
+    Route start time is set to earliest possible. TODO: Implement forward time slack to adjust start times
+    """
+    def timeFeasible(self, customer):
+        if len(self.nodes) == 1: 
+            self.time = min(customer.windowStart, customer.windowStart - inst._distanceMatrix[(self.nodes[-1].id, customer.id)]/inst.averageVelocity)
+        arrivalTime = self.time + inst._distanceMatrix[(self.nodes[-1].id, customer.id)]/inst.averageVelocity
+        return customer.windowStart <= arrivalTime and arrivalTime <= customer.windowEnd
+
+    def depotReachable(self, customer, remainingBattery):
+        return inst._distanceMatrix[(customer.id, self.depot.id)] <= remainingBattery
+
+    def getCost(self):
+        return self.distance * inst.fuelConsumptionRate
+
     def insert(self, customer):
+        self.time += inst._distanceMatrix[(self.nodes[-1].id, customer.id)] / inst.averageVelocity + customer.serviceTime
+        self.distance += inst._distanceMatrix[(self.nodes[-1].id, customer.id)]
         self.nodes.append(customer)
-        
 
