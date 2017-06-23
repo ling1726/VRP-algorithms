3f8082d3262f959c167b3ea40e0d308cd3b6422e
abegoli <variation.proto@gmail.com>
2017-06-24 01:34:21 +0200

    Added acceptance probabilities and scheduling strategies. Current strategy runs a bit longer but improves c103_21 by additional 200-400

--- LA/SimulatedAnnealing.py
+++ LA/SimulatedAnnealing.py
@@ -4,6 +4,7 @@ import logging
 import instance.instance as inst
 import Solver as terrible
 from neighbor import SimpleNeighborhood as neigh
+from random import random
 
 logger = logging.getLogger(__name__)
 
@@ -13,22 +14,39 @@ class SimulatedAnnealing(object):
         self.neighborhood = neigh.SimpleNeighborhood()
         self.routes = routes #initial solution
         self.cost = cost # initial solution
+        self.T0 = 10
+        self.maxTries = 10000
 
     def  solve(self):
         for t in range(sys.maxsize):
-            T = self.schedule(t)
-            if math.isclose(T, 0, abs_tol=0.0001): break
+            T = self.schedule_boltzmann(t)
+            if math.isclose(T, 0, abs_tol=0.0001) or t > self.maxTries: break
             newSolution = self.neighborhood.generate_neighbor(self.routes, self.cost)
-            if newSolution["newTotalCost"] < self.cost: # decide if the new solution should be used
+            delta = newSolution["newTotalCost"] - self.cost
+            if delta < 0 or random() < self.acceptance_probability_relaxed(delta, T): # decide if the new solution should be used
                 self.routes[newSolution["chosenRoutesIndexes"][0]] = newSolution["chosenRoutes"][0]
                 self.routes[newSolution["chosenRoutesIndexes"][1]] = newSolution["chosenRoutes"][1]
                 self.cost = newSolution["newTotalCost"]
         return self
 
     def schedule(self, t):
-        alpha = 0.95 
-        T0 = 10
-        return T0 * math.pow(alpha, t)
+        alpha = 0.99
+        return self.T0 * math.pow(alpha, t)
+
+    def schedule_boltzmann(self, t):
+        return self.T0 / math.log(t+2)
+
+    def acceptance_probability(self, delta, T):
+        p = 0
+        try:p = 1/(1 + math.exp(delta/T))
+        except OverflowError: pass
+        return p
+
+    def acceptance_probability_relaxed(self, delta, T):
+        p = 0
+        try: p = math.exp(-delta/T)
+        except OverflowError: pass
+        return p
 
     def neighbor(self, current):
         return self.neighborhood.generate_neighbor(current)
